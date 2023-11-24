9183270b5c38a0c00334ce15f14a9cc59a23bd41
nopcorn <nope@exmaple.com>
2023-11-23 19:44:43 -0500

    update to new template

--- action_page.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if (isset($_SERVER['HTTP_REFERER'])){
-  $referer=$_SERVER['HTTP_REFERER'];
-}else{
- $referer="https://darkpacket.ca/login.html";
-}
-header('Location: '.$referer);
-die();
